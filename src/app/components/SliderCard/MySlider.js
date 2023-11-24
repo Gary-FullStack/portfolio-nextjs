7df850b1188efc8b4aa059a36532ec0fab3665f8
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-11-24 15:10:41 -0500

    portfolio work page

---
+++ src/app/components/SliderCard/MySlider.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Slider from './SliderCard';
+import { DataArray } from '@/app/data'
+
+
+export default function MySlider() {
+
+  return (
+    <div> 
+        {
+
+            DataArray.map((item, index) => (
+
+                <div className='my-slider'>
+                
+                    <Slider item={item} index={index} />
+                </div>
+
+            ))
+        }
+    </div>    
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 'use client'
 import HomeComponent from './components/HomeComponent/HomeComponent'
 import {Hind} from 'next/font/google';
+import MySlider from './components/SliderCard/MySlider';
 
 const hind = Hind({
   subsets: ['latin'],
@@ -32,12 +33,15 @@ export default function Home() {
             </p>
 
             <p className={`max-w-2xl md:pl-[80px] px-5 text-[16px] text-[#47626D] leading-8 mt-5 ${hind.className}`}>
-            Over my twenty-one years of Active Duty Military Service I developed a host of diverse skills and attention to detail that are usefull in my current coder role. My mission focus and "can-do" mindset means I will solve your problem and offer you a best practice solution or I will tell you why its not feasible inside your goals and/or budget target.
+            Over my twenty-one years of Active Duty Military Service I developed a host of diverse skills and attention to detail that are usefull in my current coder role. My mission focus and can-do mindset means I will solve your problem and offer you a best practice solution or I will tell you why its not feasible inside your goals and/or budget target.
             </p>
                      
            
           </div>          
         </div>
+
+        <div className=""><MySlider /></div>
+
       </div>      
     </main>
   )
